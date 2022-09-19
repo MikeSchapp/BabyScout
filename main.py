5d05b2975548f73c4b58d3e206fd387214f18fcb
Michael Schappacher <m.a.schappacher@gmail.com>
2022-09-19 08:47:29 -0400

    Simplify assigning of gpio and functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from lib.pin import button_one, button_two, button_three, button_four, button_five, button_six, button_seven, button_eight
+from lib.pin import create_button, onboard_led
 import lib.scout as scout
 import time
 import uos as os
@@ -10,33 +10,36 @@ wlan_variables = retrieve_auth_variables(join_path(os.getcwd(), "secrets.json"))
 BASE_URL = wlan_variables["BASE_URL"]
 connect_to_wifi(wlan_variables=wlan_variables)
 baby_scout = connect_to_baby_buddy(base_url=BASE_URL)
+# Set default to first child in BabyBuddy, add functionality to allow for toggling between multiple children.
+child = baby_scout.children[0]
 
-button_one = button_one()
-button_two = button_two()
-button_three = button_three()
-button_four = button_four()
-button_five = button_five()
-button_six = button_six()
-button_seven = button_seven()
-button_eight = button_eight()
+
+# Create button matrix, matching gpio pin to the specific action you want to capture
+button_gpio_pins = [9,8,7,6,5,4,3,2]
+buttons = []
+button_actions = [
+    baby_scout.left_breast,
+    baby_scout.breast_feed,
+    baby_scout.right_breast,
+    baby_scout.bottle_feed,
+    baby_scout.wet_diaper,
+    baby_scout.solid_diaper,
+    baby_scout.wet_solid_diaper,
+    baby_scout.sleep
+]
+for button in button_gpio_pins:
+    buttons.append(create_button(button))
+
+
 
 while True:
-    if button_one.value():
-        baby_scout.left_breast(baby_scout.children[0])
-    if button_two.value():
-        baby_scout.breast_feed(baby_scout.children[0])
-    if button_three.value():
-        baby_scout.right_breast(baby_scout.children[0])
-    if button_four.value():
-        baby_scout.bottle_feed(baby_scout.children[0])
-    if button_five.value():
-        baby_scout.wet_diaper(baby_scout.children[0])
-    if button_six.value():
-        baby_scout.solid_diaper(baby_scout.children[0])
-    if button_seven.value():
-        baby_scout.wet_solid_diaper(baby_scout.children[0])
-    if button_eight.value():
-        baby_scout.sleep(baby_scout.children[0])
+    for index, button in enumerate(buttons):
+        if button.value():
+            onboard_led(1)
+            time.sleep(0.5)
+            onboard_led(0)
+            button_actions[index](child)
+
 
 
 
